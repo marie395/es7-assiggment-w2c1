e80a3905e473d31c8de6004b2dbe74c5406ea4c5
marie395 <Mariejaphetnambele@gmail.com>
2026-04-17 12:44:23 +0100

    Create Header.jsx

---
+++ React_js/my-app/src/Composant/Header.jsx
@@ -0,0 +1,17 @@
+//Author : Tsamo Tchinda Rooswell
+const Header = () => {
+  return (
+    <header
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "15px",
+        textAlign: "center",
+      }}
+    >
+      <h1>E-Learning Platform</h1>
+    </header>
+  );
+};
+
+export default Header;
